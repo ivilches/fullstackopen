a38cd2a184c8adf27863a9d77f90a3aa5fdf37f8
Israel Vilches <vilchesvalle@gmail.com>
2020-02-04 20:05:56 +0100

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
 import SearchFilter from './components/SearchFilter';
 import EntryForm from './components/EntryForm';
 import EntryList from './components/EntryList';
 const shortid = require('shortid');
 
+const backendPersonsUrl = 'http://localhost:3001/persons'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: shortid.generate(), name: 'Arto Hellas', number: '040-123456' },
-    { id: shortid.generate(), name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: shortid.generate(), name: 'Dan Abramov', number: '12-43-234345' },
-    { id: shortid.generate(), name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(backendPersonsUrl);
+      setPersons(result.data);
+    };
+    fetchData();
+  }, []);
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
