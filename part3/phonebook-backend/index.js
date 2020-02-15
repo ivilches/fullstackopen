dbfe16d2a2225c24dd227d8cd1ed5c543ca91f40
Israel Vilches <vilchesvalle@gmail.com>
2020-02-16 00:43:02 +0100

    Exercise 3.3: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const persons = require('./persons').persons;
+let persons = require('./persons').persons;
 
 app.get('/info', (req, res) => {
   res.send(`
@@ -18,15 +18,31 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
 
-  if (!person) {
+  if (!existsPerson(id)) {
     return res.status(404).json({
       error: 'person not found',
     })
   }
+  
+  const person = persons.find(p => p.id === id);
   res.send(JSON.stringify(person));
 });
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (!existsPerson(id)) {
+    return res.status(404).json({
+      error: 'person not found',
+    })
+  }
+
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+});
+
+const existsPerson = (id) => persons.some(p => p.id === id);
 
 const PORT = 3001;
 app.listen(PORT, () => {
