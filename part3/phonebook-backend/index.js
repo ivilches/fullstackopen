8dfc23d2773f5e3a083f63aaa146cdae5cec073d
Israel Vilches <vilchesvalle@gmail.com>
2020-05-23 16:59:14 +0200

    Exercise 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const shortid = require('shortid');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const logFormat = (tokens, req, res) => [
